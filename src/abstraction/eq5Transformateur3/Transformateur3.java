b157ef24ce09c94638a3ee41e09be70ef14ed9e1
polt.gun@gmail.com <pierrenicolas.doumer@laposte.net>
2020-04-24 20:49:18 +0200

    Gestion de différents stocks redirection des interfaces vers les classes correspondantes Petits ajouts

--- src/abstraction/eq5Transformateur3/Transformateur3.java
+++ src/abstraction/eq5Transformateur3/Transformateur3.java
@@ -2,7 +2,9 @@ package abstraction.eq5Transformateur3;
 
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import abstraction.eq8Romu.cacaoCriee.IAcheteurCacaoCriee;
 import abstraction.eq8Romu.cacaoCriee.LotCacaoCriee;
@@ -10,30 +12,43 @@ import abstraction.eq8Romu.cacaoCriee.PropositionCriee;
 import abstraction.eq8Romu.cacaoCriee.SuperviseurCacaoCriee;
 import abstraction.eq8Romu.chocolatBourse.IVendeurChocolatBourse;
 import abstraction.eq8Romu.produits.Chocolat;
+import abstraction.eq8Romu.produits.Feve;
+import abstraction.eq8Romu.ventesCacaoAleatoires.IAcheteurCacaoAleatoire;
+import abstraction.eq8Romu.ventesCacaoAleatoires.SuperviseurVentesCacaoAleatoires;
 import abstraction.fourni.Filiere;
 import abstraction.fourni.IActeur;
 import abstraction.fourni.Journal;
 import abstraction.fourni.Variable;
+@SuppressWarnings("unused")
+public class Transformateur3 implements IActeur, IAcheteurCacaoCriee, IVendeurChocolatBourse, IAcheteurCacaoAleatoire {
 
-public class Transformateur3 implements IActeur, IAcheteurCacaoCriee, IVendeurChocolatBourse {
-
-	private Variable stockFeves;
-	private Variable stockChocolat;
+	private Map<Feve, Variable> stockFeves;
+	private Map<Chocolat, Variable> stockChocolat;
 	private Integer cryptogramme;
 	private Journal journalEq5;
+	private AchatCacao acheteurCacao;
+	private AchatPate acheteurPate;
+	private VenteChocolat VendeurChocolat;
+	private Tresorerie tresorier;
 
 	public Transformateur3() {
-		this.stockFeves = new Variable(getNom() + " stock feves", this, 50);
-		this.stockChocolat = new Variable(getNom() + " stock chocolat", this, 100);
+		this.stockFeves = new HashMap<>();
+		this.stockChocolat = new HashMap<>();
+		this.stockFeves.put(Feve.FEVE_BASSE, new Variable(getNom() + " stock feves basse qualité", this, 50));
+		this.stockChocolat.put(Chocolat.CHOCOLAT_BASSE, new Variable(getNom() + " stock chocolat basse qualité", this, 50));
 		this.journalEq5 = new Journal("Eq5 activites", this);
+		this.acheteurCacao = new AchatCacao(); //needs to be filled with parameters this will work for now
+		this.acheteurPate = new AchatPate(); 
+		this.VendeurChocolat = new VenteChocolat(); 
+		this.tresorier = new Tresorerie(); 
 	}
 
 	public String getNom() {
-		return "EQ5_Whish'ocko";
+		return "Whish'oco";
 	}
 
 	public String getDescription() {
-		return "Transformateur bla bla bla";
+		return "Transformateur qui aime le chocolat.";
 	}
 
 	public Color getColor() {
@@ -60,12 +75,13 @@ public class Transformateur3 implements IActeur, IAcheteurCacaoCriee, IVendeurCh
 
 	public List<Variable> getIndicateurs() {
 		List<Variable> res = new ArrayList<Variable>();
-		res.add(this.stockFeves);
-		res.add(this.stockChocolat);
+		res.addAll(this.stockFeves.values());
+		res.addAll(this.stockChocolat.values());
 		return res;
 	}
 
 	public List<Variable> getParametres() {
+		//TODO ici devront être mis les paramètres dont je parlais (finalement ce seront des variables qu'il faudra penser à ajouter ici)
 		List<Variable> res = new ArrayList<Variable>();
 		return res;
 	}
@@ -78,48 +94,66 @@ public class Transformateur3 implements IActeur, IAcheteurCacaoCriee, IVendeurCh
 
 	public void notificationFaillite(IActeur acteur) {
 		if (this == acteur) {
-			System.out.println("I'll be back... or not... " + this.getNom());
+			System.out.println("RIP in pieces" + this.getNom());
 		} else {
 			System.out.println("Poor " + acteur.getNom() + "... We will miss you. " + this.getNom());
 		}
 	}
 
 	public void notificationOperationBancaire(double montant) {
+		String str = montant > 0 ? "On a gagné de l'argent ! " : "On a perdu de l'argent ! ";
+		this.journalEq5.ajouter(str + montant);
 	}
 
-	@Override
+	// Vente de Chocolat en bourse IVenteChocolatBourse
+	
 	public double getOffre(Chocolat chocolat, double cours) {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.VendeurChocolat.getOffre(chocolat, cours);
+		
 	}
 
-	@Override
+	
 	public void livrer(Chocolat chocolat, double quantite) {
-		// TODO Auto-generated method stub
-
+		this.stockChocolat.get(chocolat).retirer(this, quantite);
 	}
 
-	@Override
+	// Achat de cacao en criée IAcheteurCacaoCriee
+	
 	public double proposerAchat(LotCacaoCriee lot) {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.acheteurCacao.proposerAchat(lot);
 	}
 
-	@Override
+	
 	public void notifierPropositionRefusee(PropositionCriee proposition) {
-		// TODO Auto-generated method stub
-
+		this.acheteurCacao.notifierPropositionRefusee(proposition);
 	}
 
-	@Override
+	
 	public Integer getCryptogramme(SuperviseurCacaoCriee superviseur) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.acheteurCacao.getCryptogramme(superviseur);
 	}
 
-	@Override
+	
 	public void notifierVente(PropositionCriee proposition) {
-		// TODO Auto-generated method stub
-
+		this.acheteurCacao.notifierVente(proposition);
 	}
+	
+	// Achat de cacao aléatoire IAcheteurCacaoAleatoire c'est bizarre ce protocole 
+	
+	public double quantiteDesiree(double quantiteEnVente, double prix) {
+		return this.acheteurCacao.quantiteDesiree(quantiteEnVente, prix);
+	}
+
+	
+	public void quantiteLivree(double quantiteLivree) {
+		this.stockChocolat.get(null).ajouter(this, quantiteLivree);
+	}
+
+	
+	public Integer getCryptogramme(SuperviseurVentesCacaoAleatoires superviseur) {
+		
+		return superviseur == null ? Integer.valueOf(0) : this.cryptogramme ;
+	}
+	//TODO ajouter les methodes pour acheter de la pate de cacao
+
 }
