01021bfea3fbd1c9cf374053e4883f94a5b0e741
Patricia DUPUY <Patricia DUPUY@192.168.1.27>
2020-04-30 11:56:54 +0200

    ajout des notificationspropositionrefusee/acceptee

--- src/abstraction/eq5Transformateur3/AchatCacao.java
+++ src/abstraction/eq5Transformateur3/AchatCacao.java
@@ -19,29 +19,37 @@ public class AchatCacao {
 	
     //en criée
 	
+	
+	//achète le lot au prix minimum si les feves sont haut de gamme et equitables
+		//Il faut ajouter une condition sur les stocks et la trésorerie
 	public double proposerAchat(LotCacaoCriee lot) {
 		if (lot.getFeve().isEquitable() && lot.getFeve().getGamme()== Gamme.HAUTE) {
-			return lot.getPrixMinPourUneTonne() ;
+			return lot.getPrixMinPourUneTonne()*lot.getQuantiteEnTonnes() ;
 		}
-		return 0;
+		else {
+			return 0;
+		}
 	}
-//achète le lot au prix minimum si les feves sont haut de gamme et equitables
-	//Il faut ajouter une condition sur les stocks et la trésorerie
+	
+	//Ce nombre devra influencer le prix proposé dans proposer achat
+	private double NB_propositions_refusees;
 	
 	public void notifierPropositionRefusee(PropositionCriee proposition) {
-		
-
+		NB_propositions_refusees = +1;
 	}
 
 	
+	
 	public Integer getCryptogramme(SuperviseurCacaoCriee superviseur) {
-		
+		if (superviseur != 0) {
+			
+		}
 		return null;
 	}
 
-	
+	//Ne pas oblier les conséquences sur la trésorerie
 	public void notifierVente(PropositionCriee proposition) {
-		
+		NB_propositions_refusees = NB_propositions_refusees - 1;
 
 	}
 }
