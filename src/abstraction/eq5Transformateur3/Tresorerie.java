60252b12fef53c2eb5d8f970d0c5ac24196fcca1
simon <simon@LAPTOP-U5N1PQ90>
2020-04-30 12:05:53 +0200

    2e jet des méthodes

--- src/abstraction/eq5Transformateur3/Tresorerie.java
+++ src/abstraction/eq5Transformateur3/Tresorerie.java
@@ -8,43 +8,65 @@ public class Tresorerie {
      * Infos utiles à récupérer : montant compte en banque ; stock ; historique des ventes / achats ; agios / découverts 
      * ; montant max d'investissement de chaque filière ;
      * 
-     * Ces infos sont sûrement récupérées principalement dans le journal
+     *
      */
+	private Transformateur3 Acteur;
 	
-	public getMontantCompte() {
-		return 0
+	private double MontantCompte;
+	private double Agios;
+	private double FacteurPrioriteGamme;
+	
+	public Tresorerie(double MontantCompte, double Agios, double Facteur) {
+		this.MontantCompte=MontantCompte;
+		this.Agios=Agios;
+		this.FacteurPrioriteGamme=Facteur;
 	}
 	
-	public getStock() {
-		return 0
-		}
-	
-	public getHistorique() {
-		return null
+	public double getMontantCompte() {
+		return this.MontantCompte;
 	}
 	
-	public getAgios() {
-		return 0
+	public double getAgios() {
+		return this.Agios;
 	}
 	
-	public getFacteurPrioriteGamme () {
+	public double getFacteurPrioriteGamme() {
 		/**
 	     * Le facteur qui décrit la priorité que nous mettons sur le bas de gamme par rapport au haut de gamme (évolue au cours de la
 	     * simulation)
 	     */
+		return this.FacteurPrioriteGamme;
 	}
 	
-	public getInvestissementMaxBasDeGamme() {
+	public void setMontantCompte (double MontantCompte) {
+		this.MontantCompte=MontantCompte;
+	}
+	
+	public void setAgios (double Agios) {
+		this.Agios=Agios;
+	}
+	
+	public void setFacteurPrioriteGamme (double Facteur) {
+		/**
+	     * Le facteur qui décrit la priorité que nous mettons sur le bas de gamme par rapport au haut de gamme (évolue au cours de la
+	     * simulation)
+	     */
+		this.FacteurPrioriteGamme=Facteur;
+	}
+	
+	public double InvestissementMaxBasDeGamme() {
 		/**
 	     * Renvoie l'investissement maximum possible à faire dans le bas de gamme en fonction du facteur de priorité que l'on s'impose
 	     * ainsi que du montant de notre trésorerie
 	     */
+		return 0;
 	}
 	
-	public getInvestissementMaxHautDeGamme() {
+	public double InvestissementMaxHautDeGamme() {
 		/**
 	     * Renvoie l'investissement maximum possible à faire dans le haut de gamme en fonction du facteur de priorité que l'on s'impose
 	     * ainsi que du montant de notre trésorerie
 	     */
+		return 0;
 	}
 }
