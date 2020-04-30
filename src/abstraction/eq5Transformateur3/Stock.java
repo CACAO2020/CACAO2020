4dadbc53a8ba479fb57acbf42e634c7fdbb42465
francoisgoujon <francoisgoujon@mbp-de-francois>
2020-04-30 10:23:34 +0200

    # WARNING: head commit changed in the meantime
    Ajout class STOCK

---
+++ src/abstraction/eq5Transformateur3/Stock.java
@@ -0,0 +1,15 @@
+package abstraction.eq5Transformateur3;
+
+
+/** @author F. GOUJON  */
+public class Stock {
+	/**
+     * Gère les stock de fève, pate et chocolat du Transformateur3
+     * Elle est agrégé dans la classe Transformateur3.
+     */
+	
+	private Transformateur3 Acteur;
+	private 
+	
+	public 
+}
