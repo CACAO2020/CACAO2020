65f541b660247e5e0e877e610db8fd9d9b42fca8
Vincent <vincent@getting-nutz.fr>
2020-04-30 11:52:57 +0200

    ajout classe investisseur

---
+++ src/abstraction/eq2Producteur2/Investisseur.java
@@ -0,0 +1,13 @@
+package abstraction.eq2Producteur2;
+
+import java.util.List;
+
+import abstraction.eq8Romu.cacaoCriee.IVendeurCacaoCriee;
+import abstraction.eq8Romu.cacaoCriee.LotCacaoCriee;
+import abstraction.eq8Romu.cacaoCriee.PropositionCriee;
+import abstraction.fourni.IActeur;
+
+public class eq2Investisseur extends eq2Vendeur {
+
+
+}
