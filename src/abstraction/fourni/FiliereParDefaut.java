e98ce1af6b697347e0ce54d22395abd016e006f1
Léandre Berthault <Landre Berthault@DESKTOP-NNH1GQP>
2020-04-30 10:35:31 +0200

    bugs correction 1

--- src/abstraction/fourni/FiliereParDefaut.java
+++ src/abstraction/fourni/FiliereParDefaut.java
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-package abstraction.fourni;
-
-import abstraction.eq1Producteur1.Producteur1;
-import abstraction.eq2Producteur2.Producteur2;
-import abstraction.eq3Transformateur1.Transformateur1;
-import abstraction.eq4Transformateur2.Transformateur2;
-import abstraction.eq5Transformateur3.Transformateur3;
-import abstraction.eq6Distributeur1.Distributeur1;
-import abstraction.eq7Distributeur2.Distributeur2;
-import abstraction.eq8Romu.Romu;
-
-public class FiliereParDefaut extends Filiere {
-	
-	public FiliereParDefaut() {
-		super();
-		this.ajouterActeur(new Producteur1());
-		this.ajouterActeur(new Producteur2());
-		this.ajouterActeur(new Transformateur1());
-		this.ajouterActeur(new Transformateur2());
-		this.ajouterActeur(new Transformateur3());
-		this.ajouterActeur(new Distributeur1());
-		this.ajouterActeur(new Distributeur2());
-		this.ajouterActeur(new Romu());
-	}
-
-}
-=======
 package abstraction.fourni;
 
 import abstraction.eq1Producteur1.Producteur1;
@@ -90,4 +62,3 @@ public class FiliereParDefaut extends Filiere {
 	}
 
 }
->>>>>>> branch 'master' of https://github.com/CACAO2020/CACAO2020
--- src/presentation/secondaire/FenetreJournal.java
+++ src/presentation/secondaire/FenetreJournal.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package presentation.secondaire;
 
 import java.awt.BorderLayout;
@@ -24,93 +23,7 @@ import abstraction.fourni.Journal;
  * 
  * @author Romuald Debruyne
  */
-public class FenetreJournal extends JFrame {
 
-	private static final long serialVersionUID = 1L;
-	
-	private Journal journal;
-	private JLabel label;
-    private JCheckBox checkBox;
-    
-	public FenetreJournal(Journal j) {
-		super(""+j.getNom());
-		this.journal = j;
-
-		this.setLayout(new BorderLayout());
-		this.label = new JLabel();
-		this.label.setText(this.journal.toHtml());
-		this.label.addMouseListener(new MouseListener() {
-			public void mouseClicked(MouseEvent e) {
-				journal.augmenterLignesAffichables();
-				repaint();
-			}
-			public void mousePressed(MouseEvent e) {}
-			public void mouseReleased(MouseEvent e) {}
-			public void mouseEntered(MouseEvent e) {}
-			public void mouseExited(MouseEvent e) {} 
-			
-		});
-		JScrollPane sp = new JScrollPane(label);
-		this.add(sp, BorderLayout.CENTER);
-		this.setSize(new Dimension(800,600));
-		this.addWindowListener(new WindowAdapter() {public void windowClosing(WindowEvent e) {
-			FenetreJournal gg = (FenetreJournal)(e.getSource());
-			gg.setVisible(false);
-		
-		   if (gg.checkBox!=null) {gg.checkBox.setSelected(false);} 
-		   }});
-
-	}
-	
-	@Override
-	public void paint(Graphics g) {
-		this.label.setText(journal.toHtml());
-		super.paint(g);
-	}
-	
-	public void setCheckBox(JCheckBox checkBox) {
-		this.checkBox = checkBox;
-	}
-	public void setLabel(String s) {
-		this.label.setText(s);
-		if (this.isVisible()) {
-			this.repaint();
-		}
-	}
-}
-=======
-package presentation.secondaire;
-
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.swing.JCheckBox;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JScrollPane;
-import javax.swing.filechooser.FileFilter;
-
-import abstraction.fourni.Journal;
-
-/**
- * Classe modelisant une fenetre presentant graphiquement un Journal.
- * Cette classe est utilisee pour l'affichage des journaux.
- * 
- * Vous n'aurez pas, a priori, a utiliser directement cette classe
- * 
- * @author Romuald Debruyne
- */
 public class FenetreJournal extends JFrame {
 
 	private static final long serialVersionUID = 1L;
@@ -227,4 +140,3 @@ class FichiersTXT extends FileFilter {
 		return "fichiers TXT";
 	}
 }
->>>>>>> branch 'master' of https://github.com/CACAO2020/CACAO2020
