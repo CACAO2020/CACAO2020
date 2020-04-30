73f948d857ac3ecb9862e36958e7f85f4df88d5e
Rafarazh <raphael-garnier@hotmail.fr>
2020-04-30 11:47:47 +0100

    Création class EQ7Stock

---
+++ src/abstraction/eq7Distributeur2/Eq7Stock.java
@@ -0,0 +1,41 @@
+package abstraction.eq7Distributeur2;
+
+public class Eq7Stock implements IStock {
+
+	@Override
+	public double getStocks() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double quantitesRecues() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double quantitesPrelevees() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double coutStockage() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double estimationProchStock() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double stockAnneePrec() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
