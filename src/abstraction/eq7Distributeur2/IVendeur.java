158a90b4bfc7f749301c685bcf98623709ea10f0
R19caby <raphael.caby@imt-atlantique.net>
2020-04-30 11:18:59 +0200

    Création des différentes méthodes de l'interface IVendeur

--- src/abstraction/eq7Distributeur2/IVendeur.java
+++ src/abstraction/eq7Distributeur2/IVendeur.java
@@ -2,4 +2,15 @@ package abstraction.eq7Distributeur2;
 
 public interface IVendeur {
 	
+	public double demandeClient();
+	
+	public double consulterStocks();
+	
+	public double vente();
+	
+	public double envoyerClients();
+
+	public void notificationVente(double quantite, double prix);
+	
+	
 }
