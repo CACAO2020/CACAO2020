7ff79ed88686d0966ab6167c73e4fb88ab752f39
Rafarazh <raphael-garnier@hotmail.fr>
2020-04-30 11:37:13 +0100

    Ajout des premieres methodes a l'interface IStock

--- src/abstraction/eq7Distributeur2/IStock.java
+++ src/abstraction/eq7Distributeur2/IStock.java
@@ -1,5 +1,20 @@
 package abstraction.eq7Distributeur2;
 
 public interface IStock {
+	//raphael G
 
+	public double getStocks();   //informe sur les 6 stocks différents
+	
+	public double quantitesRecues(); //Indique quantitées recues par le protocole d'achat
+	
+	public double quantitesPrelevees();  //Indique quantitées prélevées (cad retirées du stock) par le protocole de vente 
+	
+	public double coutStockage();  //Indique les couts du stockage actuel 
+	
+	public double estimationProchStock(); //Estime les couts de stockage lors de la prochaine step 
+	                                      // (ie calcule la différence entre achat et vente lors de cette step et l'ajoute au stock)
+	
+	public double stockAnneePrec();    //Informe stock 1 an auparavant
+		
+	
 }
