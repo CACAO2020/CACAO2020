8b51fbd37e0638ab888c0b0784c6ed13c5c40530
Patricia DUPUY <Patricia DUPUY@192.168.1.27>
2020-04-30 11:04:36 +0200

    proposition d'achat basique

--- src/abstraction/eq5Transformateur3/AchatCacao.java
+++ src/abstraction/eq5Transformateur3/AchatCacao.java
@@ -3,30 +3,30 @@ package abstraction.eq5Transformateur3;
 import abstraction.eq8Romu.cacaoCriee.LotCacaoCriee;
 import abstraction.eq8Romu.cacaoCriee.PropositionCriee;
 import abstraction.eq8Romu.cacaoCriee.SuperviseurCacaoCriee;
+import abstraction.eq8Romu.produits.Gamme;
 
 public class AchatCacao {
     /**
      * Classe qui régit tous les achats de fèves, à la criée et aléatoire
      * Elle est agrégé dans la classe Transformateur3.
      */
-    //aléatoire
- 	
-	public double quantiteDesiree(double quantiteEnVente, double prix) {
-		
+    //aléatoire	
+	public double quantiteDesiree(double quantiteEnVente, double prix) {	
 		return 0;
 	}
-
-	
 	public void quantiteLivree(double quantiteLivree) {
-		
-
 	}
+	
     //en criée
+	
 	public double proposerAchat(LotCacaoCriee lot) {
-		
+		if (lot.getFeve().isEquitable() && lot.getFeve().getGamme()== Gamme.HAUTE) {
+			return lot.getPrixMinPourUneTonne() ;
+		}
 		return 0;
 	}
-
+//achète le lot au prix minimum si les feves sont haut de gamme et equitables
+	//Il faut ajouter une condition sur les stocks et la trésorerie
 	
 	public void notifierPropositionRefusee(PropositionCriee proposition) {
 		
