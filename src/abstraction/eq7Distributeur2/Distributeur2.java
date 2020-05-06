af0eea8060b5a339ee6103266310e80414fb9ac0
Léandre Berthault <Landre Berthault@DESKTOP-NNH1GQP>
2020-05-06 16:33:46 +0200

    Commit

--- src/abstraction/eq7Distributeur2/Distributeur2.java
+++ src/abstraction/eq7Distributeur2/Distributeur2.java
@@ -1,43 +1,78 @@
 package abstraction.eq7Distributeur2;
 
-import abstraction.eq8Romu.clients.ClientFinal;
-import abstraction.eq8Romu.clients.IDistributeurChocolat;
-import abstraction.eq8Romu.produits.Chocolat;
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * 
- * @author R. Debryune
- *
- * Un exemple simpliste d'un distributeur qui ne commercialise qu'une variete de chocolat, 
- * qui peut au plus mettre capaciteDeVente tonnes de chocolat en vente par step, et qui a
- * un prix fixe pour ce chocolat 
- */
-public class Distributeur2 extends DistributeurChocolatAbs implements IDistributeurChocolat {
+import abstraction.fourni.Filiere;
+import abstraction.fourni.IActeur;
+import abstraction.fourni.Journal;
+import abstraction.fourni.Variable;
 
-	private double capaciteDeVente;
-	private double prix;
+public class Distributeur2 implements IActeur {
 
-	public Distributeur2(Chocolat choco, double capaciteDeVente, double prix) {
-		super(choco);
-		this.capaciteDeVente = capaciteDeVente;
-		this.prix = prix;
+	private Integer cryptogramme;
+	private Journal journalEq7;
+
+	public Distributeur2() {
+		this.journalEq7 = new Journal("Eq7 activites", this);
 	}
 
-	public boolean commercialise(Chocolat choco) {
-		return (this.chocolat.equals(choco)); // ne commercialise qu'un seul chocolat
+	public String getNom() {
+		return "EQ7";
 	}
 
-	public double quantiteEnVente(Chocolat choco) {
-		return Math.min(capaciteDeVente, stockChocolat.getValeur());
+	public String getDescription() {
+		return "Ecocoa de Liège";
 	}
 
-	public double prix(Chocolat choco) {
-		return this.prix;
+	public Color getColor() {
+		return new Color(230, 126, 34);
 	}
 
-	public void vendre(ClientFinal client, Chocolat choco, double quantite) {
-		if (client!=null) { // permet de verifier que c'est bien un client final qui appelle cette methode car les autres acteurs n'ont aucun moyen de connaitre la reference vers l'unique instance de ClientFinal
-			stockChocolat.retirer(client, quantite);
+	public void initialiser() {
+	}
+
+	public void setCryptogramme(Integer crypto) {
+		this.cryptogramme = crypto;
+	}
+
+	public void next() {
+	}
+
+	public List<String> getNomsFilieresProposees() {
+		return new ArrayList<String>();
+	}
+
+	public Filiere getFiliere(String nom) {
+		return null;
+	}
+
+	public List<Variable> getIndicateurs() {
+		List<Variable> res=new ArrayList<Variable>();
+		return res;
+	}
+
+	public List<Variable> getParametres() {
+		List<Variable> res=new ArrayList<Variable>();
+		res.add(this.margeMin);
+		return res;
+	}
+
+	public List<Journal> getJournaux() {
+		List<Journal> res=new ArrayList<Journal>();
+		res.add(journalEq6);
+		return res;
+	}
+
+	public void notificationFaillite(IActeur acteur) {
+		if (this==acteur) {
+			System.out.println("I'll be back... or not... "+this.getNom());
+		} else {
+			System.out.println("Poor "+acteur.getNom()+"... We will miss you. "+this.getNom());
 		}
+	}
+
+	public void notificationOperationBancaire(double montant) {
 	}
 }
