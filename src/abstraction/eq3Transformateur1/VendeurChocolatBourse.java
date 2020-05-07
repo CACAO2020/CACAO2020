9171a81ca61a7afcd43c887c3b97f0f76e062bdf
karlgutierrez <karlgutierrez@mackarl.home>
2020-05-07 10:35:10 +0200

    VendeurChocolatBourse

--- src/abstraction/eq3Transformateur1/VendeurChocolatBourse.java
+++ src/abstraction/eq3Transformateur1/VendeurChocolatBourse.java
@@ -1,19 +1,37 @@
 package abstraction.eq3Transformateur1;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import abstraction.eq8Romu.chocolatBourse.IVendeurChocolatBourse;
-import abstraction.eq8Romu.produits.Chocolat;;
+import abstraction.eq8Romu.produits.Chocolat;
+import abstraction.eq8Romu.produits.Gamme;
+import abstraction.fourni.Journal;
+import abstraction.fourni.Variable;;
 public abstract class VendeurChocolatBourse extends Stock implements IVendeurChocolatBourse {
 
-	@Override
+	private static int NB_INSTANCES = 0;
+	private int numeroVendeur;
+	private Chocolat chocolat;
+	
+	/** @author K. GUTIERREZ  */
+	public VendeurChocolatBourse(Chocolat chocolat) {	
+		if (chocolat==null) {
+			throw new IllegalArgumentException("chocolat==null");
+		}		
+		NB_INSTANCES++;
+		this.numeroVendeur = NB_INSTANCES;
+		this.chocolat = chocolat;
+	}
+	
+	/** @author K. GUTIERREZ  */
 	public double getOffre(Chocolat chocolat, double cours) {
-		// TODO Auto-generated method stub
-		
-		return 0;
+		return cours*((Variable) StockChocolat).getValeur();
 	}
 
-	@Override
+	/** @author K. GUTIERREZ  */
 	public void livrer(Chocolat chocolat, double quantite) {
-		// TODO Auto-generated method stub
-		
+		((Variable) StockChocolat).retirer(this, quantite);
 	}
 
 }
