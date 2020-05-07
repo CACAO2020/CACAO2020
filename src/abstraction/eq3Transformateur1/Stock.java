30487654278f2db5ad0031febdc62e19da2402a8
ACdr <ACdr@192.168.1.17>
2020-05-07 10:47:59 +0200

    changement nom variable

--- src/abstraction/eq3Transformateur1/Stock.java
+++ src/abstraction/eq3Transformateur1/Stock.java
@@ -6,31 +6,35 @@ import java.util.Map;
 
 import abstraction.eq8Romu.produits.Chocolat;
 import abstraction.eq8Romu.produits.Feve;
-import abstraction.eq8Romu.produits.Gamme;
 import abstraction.fourni.Variable;
+
 
 public class Stock extends ActeurEQ3 {
 	/*
 	 * Author Amaury Coudray
 	 */
 
-	protected Map<Feve,Double> StockFeves;
-	protected Map<Chocolat,Double> StockChocolat;
+	protected Map<Feve,Double> stockFeves;
+	protected Map<Chocolat,Double> stockChocolat;
+	protected Variable stockTotalFeves;
+	protected Variable stockTotalChocolat;
 	public Stock() {
-		this.StockFeves=new HashMap<Feve,Double>();
-		this.StockChocolat=new HashMap<Chocolat,Double>();
+		this.stockFeves=new HashMap<Feve,Double>();
+		this.stockChocolat=new HashMap<Chocolat,Double>();
+		this.stockTotalFeves=new Variable("stock total de feves de "+getNom(),this,0);
+		
 	}
 	
 	public void setStockFeves(Feve feve,Double quantite) {
-		this.StockFeves.put(feve,quantite+this.StockFeves.get(feve));
+		this.stockFeves.put(feve,quantite+this.stockFeves.get(feve));
 	}
 	public void setStockChocolat(Chocolat chocolat,Double quantite) {
-		this.StockChocolat.put(chocolat,quantite+this.StockChocolat.get(chocolat));
+		this.stockChocolat.put(chocolat,quantite+this.stockChocolat.get(chocolat));
 	}
 	public Map<Feve,Double> getStockFeves() {
-		return this.StockFeves;
+		return this.stockFeves;
 	}
 	public Map<Chocolat,Double> getStockChocolat() {
-		return this.StockChocolat;
+		return this.stockChocolat;
 	}
 }
