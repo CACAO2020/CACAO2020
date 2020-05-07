0e981c47d50e4eddaec39189a2a24c98a74a71b7
ACdr <ACdr@192.168.1.17>
2020-05-07 09:40:33 +0200

    Ajout de setStockFeve a stock

--- src/abstraction/eq3Transformateur1/Stock.java
+++ src/abstraction/eq3Transformateur1/Stock.java
@@ -1,5 +1,29 @@
 package abstraction.eq3Transformateur1;
 
+import java.util.List;
+
+import abstraction.eq8Romu.produits.Chocolat;
+import abstraction.eq8Romu.produits.Feve;
+import abstraction.eq8Romu.produits.Gamme;
+
 public class Stock extends AchteurCacao {
+	/*
+	 * Author Amaury Coudray
+	 */
+	private List<Double> ListStockCacao;
+	private List<Double> ListStockChocolat;
+	/*
+	 * ListStockCacao 
+	 * Index:	
+	 * 0=>CHOCOLAT_BASSE(Gamme.BASSE, false, false),
+	 * 1=>CHOCOLAT_MOYENNE(Gamme.MOYENNE, false, false),
+	 * 2=>CHOCOLAT_HAUTE(Gamme.HAUTE, false, false),
+	 * 3=>CHOCOLAT_MOYENNE_EQUITABLE(Gamme.MOYENNE, true, false),
+	 * 4=>CHOCOLAT_HAUTE_EQUITABLE(Gamme.HAUTE, true, false);
+	 */
+	public void setStockFeve(Feve feve,Double quantite) {
+		
+	}
+	
 
 }
