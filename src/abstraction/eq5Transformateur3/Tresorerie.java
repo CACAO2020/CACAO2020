75b97853ba14da55eb5ae11fcb4bae53c7919aae
simon <simon@LAPTOP-U5N1PQ90>
2020-05-07 12:23:29 +0200

    Ajout paramètres decouverts/agios, méthode modifiant trésorerie

--- src/abstraction/eq5Transformateur3/Tresorerie.java
+++ src/abstraction/eq5Transformateur3/Tresorerie.java
@@ -1,4 +1,9 @@
 package abstraction.eq5Transformateur3;
+
+import abstraction.fourni.Banque;
+import abstraction.fourni.Filiere;
+import abstraction.fourni.Variable;
+
 /** @author Simon MORO  */
 public class Tresorerie {
     /**
@@ -8,26 +13,58 @@ public class Tresorerie {
      * Infos utiles à récupérer : montant compte en banque ; stock ; historique des ventes / achats ; agios / découverts 
      * ; montant max d'investissement de chaque filière ;
      * 
-     *
+     *structure d'une Variable = (String nom, IActeur createur,  double min, double max, double valInit)
      */
 	private Transformateur3 Acteur;
-	
+	private Banque banque; //Filiere.LA_FILIERE.getBanque()
 	private double MontantCompte;
-	private double Agios;
-	private double FacteurPrioriteGamme;
+	private double Decouvert;
+	private double FacteurPrioriteGamme; // 100% haute gamme = 1, 100% bas de gamme = 0
+
 	
-	public Tresorerie(double MontantCompte, double Agios, double Facteur) {
+	private Variable decouvertsConsecutifsAvantFaillite; //parametres fixes à priori
+	private Variable decouvertAutorise;
+	private Variable agiosDecouvertAutorise;
+	private Variable agiosDecouvertAuDela;
+	private Variable seuilOperationsRefusees;
+	
+	public Tresorerie(Transformateur3 acteur, Banque banque, double MontantCompte, double Decouvert, double Facteur, Variable decouvertsConsecutifsAvantFaillite,
+			Variable decouvertAutorise, Variable agiosDecouvertAutorise, Variable agiosDecouvertAuDela, Variable seuilOperationsRefusees) {
+		this.Acteur = acteur;
+		this.banque = banque;
 		this.MontantCompte=MontantCompte;
-		this.Agios=Agios;
+		this.Decouvert=Decouvert;
 		this.FacteurPrioriteGamme=Facteur;
+		
+		this.decouvertsConsecutifsAvantFaillite = decouvertsConsecutifsAvantFaillite;
+		this.decouvertAutorise = decouvertAutorise;
+		this.agiosDecouvertAutorise = agiosDecouvertAutorise;
+		this.agiosDecouvertAuDela = agiosDecouvertAuDela;
+		this.seuilOperationsRefusees = seuilOperationsRefusees;
+	}
+	
+	public Tresorerie(Transformateur3 acteur) {
+		/**
+	     * Initialise la trésorerie
+	     */
+		this(acteur,
+				Filiere.LA_FILIERE.getBanque(),
+				0,									//montantCompte
+				0,									//decouvert actuel
+				0,									//FacteurPriorite
+				Filiere.LA_FILIERE.getBanque().getParametres().get(0),
+				Filiere.LA_FILIERE.getBanque().getParametres().get(1),
+				Filiere.LA_FILIERE.getBanque().getParametres().get(2),
+				Filiere.LA_FILIERE.getBanque().getParametres().get(3),
+				Filiere.LA_FILIERE.getBanque().getParametres().get(4));
 	}
 	
 	public double getMontantCompte() {
 		return this.MontantCompte;
 	}
 	
-	public double getAgios() {
-		return this.Agios;
+	public double getDecouvert() {
+		return this.Decouvert;
 	}
 	
 	public double getFacteurPrioriteGamme() {
@@ -42,8 +79,8 @@ public class Tresorerie {
 		this.MontantCompte=MontantCompte;
 	}
 	
-	public void setAgios (double Agios) {
-		this.Agios=Agios;
+	public void setDecouvert (double Decouvert) {
+		this.Decouvert=Decouvert;
 	}
 	
 	public void setFacteurPrioriteGamme (double Facteur) {
@@ -68,5 +105,13 @@ public class Tresorerie {
 	     * ainsi que du montant de notre trésorerie
 	     */
 		return 0;
+	}
+	
+	public void DiminueTresorerie(double montant) {
+		
+	}
+	
+	public void AugmenteTresorerie(double montant) {
+		
 	}
 }
