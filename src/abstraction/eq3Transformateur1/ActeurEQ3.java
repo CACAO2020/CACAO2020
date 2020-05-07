25c81231090d9529d90b67312e57483b2c092f59
theop <theop@DELL-THEO>
2020-05-07 10:59:35 +0200

    ActeurEQ3

--- src/abstraction/eq3Transformateur1/ActeurEQ3.java
+++ src/abstraction/eq3Transformateur1/ActeurEQ3.java
@@ -3,16 +3,18 @@ import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 
+import abstraction.eq8Romu.cacaoCriee.FiliereTestVentesCacaoCriee;
+import abstraction.eq8Romu.ventesCacaoAleatoires.FiliereVentesCacaoAleatoires;
 import abstraction.fourni.Filiere;
 import abstraction.fourni.IActeur;
 import abstraction.fourni.Journal;
 import abstraction.fourni.Variable;
 
 public class ActeurEQ3 implements IActeur{
-	private Variable stockFeves;
-	private Variable stockChocolat;
-	private Integer cryptogramme;
-	private Journal journalEq3;
+	protected Variable stockFeves;
+	protected Variable stockChocolat;
+	protected Integer cryptogramme;
+	protected Journal journalEq3;
 
 	public ActeurEQ3() {
 		this.stockFeves=new Variable(getNom()+" stock feves", this, 50);
@@ -25,7 +27,12 @@ public class ActeurEQ3 implements IActeur{
 	}
 
 	public String getDescription() {
-		return "Transformateur bla bla bla";
+		return "eq3Transformateur1 transforme du cacao en chocolat de moyenne et haute gamme, labellisé équitable ou non."
+				+ "Le centre de notre activité concernera la transformation du chocolat haut de gamme."
+				+ "La transformation du chocolat haut de gamme est effectuée totalement en interne depuis l'achat de la fève jusqu'à la revente du produit finalisé."
+				+ "Sur le haut de gamme la majorité de la production sera du chocolat labellisé."
+				+ "Concernant la gamme moyenne, du chocolat labellisé équitable ou non sera produit.";
+		
 	}
 	
 	public Color getColor() {
@@ -42,11 +49,18 @@ public class ActeurEQ3 implements IActeur{
 	}
 
 	public List<String> getNomsFilieresProposees() {
-		return new ArrayList<String>();
+		ArrayList<String> filieres = new ArrayList<String>();
+		filieres.add("ACHATCACAOCRIEE");
+		filieres.add("VENTECHOCOLATBOURSE");
+		return filieres;
 	}
 
 	public Filiere getFiliere(String nom) {
-		return null;
+		switch (nom) {
+		case "ACHATCACAOCRIEE" : return new FiliereVentesCacaoAleatoires();
+		case "VENTECHOCOLATBOURSE" : return new FiliereTestVentesCacaoCriee();
+		default : return null;
+		}
 	}
 	
 	public List<Variable> getIndicateurs() {
