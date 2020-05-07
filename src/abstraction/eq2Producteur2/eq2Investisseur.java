0357432d9a3481cd4795e3b9fdbe78c6b1888259
Vincent <vincent@getting-nutz.fr>
2020-05-07 11:17:54 +0200

    test

--- src/abstraction/eq2Producteur2/eq2Investisseur.java
+++ src/abstraction/eq2Producteur2/eq2Investisseur.java
@@ -5,17 +5,25 @@ import java.util.List;
 import abstraction.eq8Romu.cacaoCriee.IVendeurCacaoCriee;
 import abstraction.eq8Romu.cacaoCriee.LotCacaoCriee;
 import abstraction.eq8Romu.cacaoCriee.PropositionCriee;
+import abstraction.fourni.Filiere;
 import abstraction.fourni.IActeur;
 import abstraction.fourni.Variable;
 
 public class eq2Investisseur extends eq2Vendeur {
 
-	public eq2Investisseur(IActeur createur, double init1, double init2, double init3, double init4, double init5,
-			Variable prixTF, Variable prixTT, Variable prixTC, Variable prixTPBG, Variable prixTPHG) {
+	private Variable prixArbre; 
+
+	public eq2Investisseur(IActeur createur, double init1, double init2, double init3, double init4, double init5, Variable prixTF, Variable prixTT, Variable prixTC, Variable prixTPBG, Variable prixTPHG, Variable prixArbre) {
 		super(createur, init1, init2, init3, init4, init5, prixTF, prixTT, prixTC, prixTPBG, prixTPHG);
+		this.prixArbre = prixArbre;
 		// TODO Auto-generated constructor stub
 	}
 
+	public void AchatArbre(){
+		if (Filiere.LA_FILIERE.getBanque().getSolde()) {
 
+		}
+	}
+	
 
 }
