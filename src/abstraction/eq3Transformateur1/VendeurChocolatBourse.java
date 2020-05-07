8bd5ef78925296a276609222f6cb4b7d83df1247
karlgutierrez <karlgutierrez@mackarl.home>
2020-05-07 09:20:36 +0200

    VendeurChocolatBourse

--- src/abstraction/eq3Transformateur1/VendeurChocolatBourse.java
+++ src/abstraction/eq3Transformateur1/VendeurChocolatBourse.java
@@ -1,11 +1,12 @@
 package abstraction.eq3Transformateur1;
 import abstraction.eq8Romu.chocolatBourse.IVendeurChocolatBourse;
 import abstraction.eq8Romu.produits.Chocolat;;
-public class VendeurChocolat extends Stock implements IVendeurChocolatBourse {
+public abstract class VendeurChocolatBourse extends Stock implements IVendeurChocolatBourse {
 
 	@Override
 	public double getOffre(Chocolat chocolat, double cours) {
 		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
