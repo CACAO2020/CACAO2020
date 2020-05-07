07acda1bb876efa401e9027923171ed705565884
nanou <nanou@LAPTOP-EECE8O1D.home>
2020-05-07 10:49:58 +0200

    Fin de l'implémentation de l'interface suppresion de AcheteurCacaoCriee

--- src/abstraction/eq3Transformateur1/AcheteurCacao.java
+++ src/abstraction/eq3Transformateur1/AcheteurCacao.java
@@ -14,33 +14,23 @@ import abstraction.fourni.IActeur;
 import abstraction.fourni.Journal;
 import abstraction.fourni.Variable;
 
-public class AcheteurCacao extends ActeurEQ3 implements abstraction.eq8Romu.cacaoCriee.IAcheteurCacaoCriee{
+public class AcheteurCacao extends Stock implements abstraction.eq8Romu.cacaoCriee.IAcheteurCacaoCriee{
 	private static int NB_INSTANCES = 0; // Afin d'attribuer un nom different a toutes les instances
 	private int numero;
-	private Variable totalStocksFeves;
-	protected Map<Feve, Double> stocksFeves;
-	protected Integer cryptogramme;
-	protected Journal journal;
 
 	public AcheteurCacao() {
 		NB_INSTANCES++;
 		this.numero=NB_INSTANCES;
-		this.totalStocksFeves=new Variable(getNom()+" total stocks feves", this, 50);
-		stocksFeves=new HashMap<Feve, Double>();
-		for (Feve feve : Feve.values()) {
-			stocksFeves.put(feve, 0.0);
-		}
-		this.journal = new Journal(this.getNom()+" activites", this);
-}
+	}
 	
 	public void next() {
 		double total=0.0;
 		for (Feve feve :Feve.values()) {
-			if (stocksFeves.get(feve)!=null) {
-				total=total+stocksFeves.get(feve);
+			if (stockFeves.get(feve)!=null) {
+				total=total+stockFeves.get(feve);
 			}
 		}
-		this.totalStocksFeves.setValeur(this, total);
+		this.stockTotalFeves.setValeur(this, total);
 	}
 
 /** On ne souhaite qu'acheter des fèves haut de gamme, haut de gamme équitable ou moyenne gamme équitable, on utilise des prix arbitraire pour l'instant
@@ -53,21 +43,30 @@ public class AcheteurCacao extends ActeurEQ3 implements abstraction.eq8Romu.caca
 			return 15.0;}
 		return 0.0;
 	}
-
+/** On fait un simple print de la notification de vente ou de refus de vente, on pourra ajouter cette notification au journal quand l'attribut journal sera mis en protected et qu'on y aura accès dans cette classe
+ Nathan Olborski
+ */
 	public void notifierPropositionRefusee(PropositionCriee proposition) {
-			System.out.println("La proposition " + proposition.getLot() + ", de " + proposition.getAcheteur() + ", au prix" + proposition.getPrixPourLeLot()
+			System.out.println("La proposition " + proposition.getLot().toString() + ", de " + proposition.getVendeur().toString() + ", au prix" + proposition.getPrixPourLeLot()
 					+ "a été refusée");
 	}
+	
+	public void notifierVente(PropositionCriee proposition) {
+		this.setStockFeves(proposition.getLot().getFeve(), proposition.getQuantiteEnTonnes());
+		System.out.println("Lot " + proposition.getLot().toString() + ", de " + proposition.getVendeur().toString() + ", au prix" + proposition.getPrixPourLeLot() + " a été effectué");
+		
+	}
 
+/** Simple getter, on voit ici l'intérêt d'avoir nos variables en protected 
+ Nathan Olborski
+ */
 	public Integer getCryptogramme(SuperviseurCacaoCriee superviseur) {
 		if (superviseur != null) {
 			return this.cryptogramme;}
 		else return null;
 	}
 
-	public void notifierVente(PropositionCriee proposition) {
-		this.			
-	}
+
 
 
 }
