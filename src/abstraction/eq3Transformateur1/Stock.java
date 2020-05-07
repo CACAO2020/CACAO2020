fa4e91c983ebffd045eb7575e87c0c3b685a620f
ACdr <ACdr@192.168.1.17>
2020-05-07 10:17:00 +0200

    changement extends

--- src/abstraction/eq3Transformateur1/Stock.java
+++ src/abstraction/eq3Transformateur1/Stock.java
@@ -1,28 +1,31 @@
 package abstraction.eq3Transformateur1;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import abstraction.eq8Romu.produits.Chocolat;
 import abstraction.eq8Romu.produits.Feve;
 import abstraction.eq8Romu.produits.Gamme;
+import abstraction.fourni.Variable;
 
-public class Stock extends AchteurCacao {
+public class Stock extends ActeurEQ3 {
 	/*
 	 * Author Amaury Coudray
 	 */
-	private List<Double> ListStockCacao;
-	private List<Double> ListStockChocolat;
-	/*
-	 * ListStockCacao 
-	 * Index:	
-	 * 0=>CHOCOLAT_BASSE(Gamme.BASSE, false, false),
-	 * 1=>CHOCOLAT_MOYENNE(Gamme.MOYENNE, false, false),
-	 * 2=>CHOCOLAT_HAUTE(Gamme.HAUTE, false, false),
-	 * 3=>CHOCOLAT_MOYENNE_EQUITABLE(Gamme.MOYENNE, true, false),
-	 * 4=>CHOCOLAT_HAUTE_EQUITABLE(Gamme.HAUTE, true, false);
-	 */
-	public void setStockFeve(Feve feve,Double quantite) {
-		
+
+	private Map<Feve,Double> StockFeves;
+	private Map<Chocolat,Double> StockChocolat;
+	public Stock() {
+		StockFeves=new HashMap<Feve,Double>();
+		StockChocolat=new HashMap<Chocolat,Double>();
+	}
+	
+	public void setStockFeves(Feve feve,Double quantite) {
+		StockFeves.put(feve,quantite+StockFeves.get(feve));
+	}
+	public void setStockChocolat(Chocolat chocolat,Double quantite) {
+		StockChocolat.put(chocolat,);
 	}
 	
 
