facfae27cd451f7e6bfd9fa6b39dd4d4f8435e90
Patricia DUPUY <Patricia DUPUY@ibmT400.home>
2020-05-15 12:18:47 +0200

    erreurs

--- src/abstraction/eq5Transformateur3/AchatPate.java
+++ src/abstraction/eq5Transformateur3/AchatPate.java
@@ -15,10 +15,37 @@ public class AchatPate {
     public AchatPate(Transformateur3 acteur) {
         this.acteur = acteur;
     }
+    public Echeancier contrePropositionDeLAcheteur(ExemplaireContratCadre contrat) {
+		
+		//on releve les parametres importants de l'offre du vendeur
+		int stepDeb = contrat.getEcheancier().getStepDebut();
+		double quantite = contrat.getEcheancier().getQuantite(step);
+		//notre offre initial
+		int stepDeb0 = contrat.getEcheancier().get(0).getStepDebut();
+		double quantite0 = contrat.getEcheancier().get(0).getQuantite(step);
+		
+		//si on est d'accord avec la proposition:
+		if(stepDeb == stepDeb0 && Math.abs(quantite - quantite0) < 0.25*quantite0) {
+			// la date intitile gardee et l'écart est inférieur à 25% de la quantité demandée
+			return contrat.getEcheancier();
+		}
+		
+		//sinon, on peut soit arreter les negociations, soit reproproposer
+		else if(/*acheteur met fin aux négo*/) {
+			return null;
+			}
+		
+		else {
+			if(quantite-quantite0<0) {
+				return new Echeancier(stepDeb0,contrat.getEcheancier().getStepFin(),quantite0*(1-0.25));
+			}
+			else {
+				return new Echeancier( stepDeb0, contrat.getEcheancier().getStepFin(), quantite0*(1+0.25));
+			}
+			}
+	};
 
-	public Echeancier contrePropositionDeLAcheteur(ExemplaireContratCadre contrat) {
-		return null;
-	}
+
 
 	public double contrePropositionPrixAcheteur(ExemplaireContratCadre contrat) {
 		return 0;
