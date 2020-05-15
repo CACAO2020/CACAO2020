f031c49e9af98370b8e3ac8fd6186f217f446f11
Patricia DUPUY <Patricia DUPUY@ibmT400.home>
2020-05-15 12:12:30 +0200

    debut contrat cadre

--- src/abstraction/eq8Romu/contratsCadres/IAcheteurContratCadre.java
+++ src/abstraction/eq8Romu/contratsCadres/IAcheteurContratCadre.java
@@ -17,7 +17,35 @@ public interface IAcheteurContratCadre extends IActeur {
 	 * si l'acheteur est d'accord pour un tel echeancier. Sinon, retourne un nouvel echeancier
 	 * que le superviseur soumettra au vendeur.
 	 */
-	public Echeancier contrePropositionDeLAcheteur(ExemplaireContratCadre contrat);
+	public Echeancier contrePropositionDeLAcheteur(ExemplaireContratCadre contrat) {
+		
+		//on releve les parametres importants de l'offre du vendeur
+		new int stepDeb = contrat.getEcheancier().getStepDebut();
+		new double quantite = contrat.getEcheancier().getQuantite(step);
+		//notre offre initial
+		new int stepDeb0 = contrat.getEcheancier().get(0).getStepDebut();
+		new double quantite0 = contrat.getEcheancier().get(0).getQuantite(step);
+		
+		//si on est d'accord avec la proposition:
+		if(stepDeb == stepDeb0 && Math.abs(quantite - quantite0) < 0.25*quantite0) {
+			// la date intitile gardee et l'écart est inférieur à 25% de la quantité demandée
+			return contrat.getEcheancier();
+		}
+		
+		//sinon, on peut soit arreter les negociations, soit reproproposer
+		else if(/*acheteur met fin aux négo*/) {
+			return null;
+			}
+		
+		else {
+			if(quantite-quantite0<0) {
+				return Echeancier(int stepDeb0, int contrat.getEcheancier().getStepFin(), double quantite0*(1-0.25));
+			}
+			else {
+				return Echeancier(int stepDeb0, int contrat.getEcheancier().getStepFin(), double quantite0*(1+0.25));
+			}
+			}
+	};
 
 	/**
 	 * Methode appelee par le SuperviseurVentesContratCadre lors des negociations sur
