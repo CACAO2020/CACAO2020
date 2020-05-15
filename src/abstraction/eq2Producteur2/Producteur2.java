ea0d7122532f65aa1121033730acf0a586e0bc49
lucas <lucas@LAPTOP-A41S86LO>
2020-05-15 11:53:52 +0200

    maj des parametres

--- src/abstraction/eq2Producteur2/Producteur2.java
+++ src/abstraction/eq2Producteur2/Producteur2.java
@@ -71,5 +71,10 @@ public class Producteur2 extends eq2Investisseur implements IActeur {
 		}
 		return res;
 	}
+	public List<Variable> getParametres(){
+		List<Variable> res=new ArrayList<Variable>();
+		res.add(new Variable("production_arbres",this,this.getPaquetsArbres().get(0).getProdmax()));
+		return res;
+	}
 
 }
