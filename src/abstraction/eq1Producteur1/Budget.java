df2a6242ff8f38d2f516c324c3a80dabee04da07
ThalieB <thalie.borderon@imt-atlantique.net>
2020-05-15 11:57:43 +0200

    Fonctions cyclique et investissement du budget

--- src/abstraction/eq1Producteur1/Budget.java
+++ src/abstraction/eq1Producteur1/Budget.java
@@ -1,5 +1,38 @@
 package abstraction.eq1Producteur1;
 
+import java.util.ArrayList;
+
+/*
+ * Cree par Thalie
+ */
+
+/*
+ * Donnees :
+ * On paie un employe (recolteur, pas de diversification des employes pour l'instant)
+ * 65 000 francs CFA par mois, ce qui correspond a 100 euros.
+ * Un nouveau plant coute 200 francs CFA (0.3), on compte 1 111 plants par hectares
+ * et chaque recolteur s'occupe de 2 hectares.
+ */
+
+
+/*
+ * Programmes disponibles :
+ * double getFonds()
+ * int getEmployes()
+ * 
+ * double setFonds(double)
+ * double setEmployes(int)
+ * double addFonds(double)
+ * double removeFonds(double)
+ * int addEmployes(int)
+ * int removeEmployes(int)
+ * 
+ * ArrayList<Integer> investissement(double, int, double, double)
+ * ArrayList<Integer> cyclique(int, double, double)
+ */
+
+
+
 public class Budget {
 	private double fonds;
 	private int employes;
@@ -35,14 +68,37 @@ public class Budget {
 	public void removeEmployes(int retrait) {
 		this.setEmployes(this.getEmployes() - retrait);
 	}
+
+
+	public ArrayList<Integer> investissement(double somme, int nb_arbres, double quantiteVendueF, double quantiteVendueT) {
+		int nb_hectares = (int) nb_arbres/1111;
+		int parite = nb_hectares - ((int) nb_hectares/2);
+		double coutArbresHectare = 333.33;
+		int newHectares = 0;
+		while (somme>(coutArbresHectare + (1-parite)*50)) {
+			newHectares += 1;
+			if (parite==0) {
+				somme = somme - coutArbresHectare - 50;
+				parite = 1;
+			} else {
+				somme = somme - coutArbresHectare;
+				parite = 0;
+			}
+		}
+		double quantiteVendue = quantiteVendueF + quantiteVendueT;
+		double proportionT = quantiteVendueT/quantiteVendue;
+		int newHectaresT = (int) proportionT*newHectares;
+		int newHectaresF = newHectares - newHectaresT;
+		ArrayList<Integer> newPlants = new ArrayList<Integer>();
+		newPlants.add(newHectaresF);
+		newPlants.add(newHectaresT);
+		return newPlants;
+	}
+		
 	
 	
-/*
- * Fonction à faire tourner à chaque cycle dans le programme principal.
- * Paie moyenne des employés (récolteurs, voir après si on diversifie les employés) :
- *  65 000 francs CFA/mois, ce qui fait environ 100 euros par mois
- */
-	public void cyclique() {
+
+	public ArrayList<Integer> cyclique(int nb_arbres, double quantiteVendueF, double quantiteVendueT) {
 		if (this.getFonds()>this.getEmployes()*50) {
 			this.removeFonds(this.getEmployes()*50);
 		} else {
@@ -50,6 +106,14 @@ public class Budget {
 			this.setEmployes(max_employes);
 			this.removeFonds(this.getEmployes()*50);
 		}
+		ArrayList<Integer> newPlants = new ArrayList<Integer>();
+		if (this.getFonds()>this.getEmployes()*25) {
+			newPlants = investissement((this.getFonds()-this.getEmployes()*25), nb_arbres, quantiteVendueF, quantiteVendueT);
+		} else {
+			newPlants.add((Integer) 0);
+			newPlants.add((Integer) 0);
+		}
+		return newPlants;
 	}
 
 }
--- src/abstraction/eq1Producteur1/Plantations.java
+++ src/abstraction/eq1Producteur1/Plantations.java
@@ -6,7 +6,7 @@ import java.util.Random;
 
 
 /*
- * Créé par Thalie
+ * Cree par Thalie
  */
 
 /*
