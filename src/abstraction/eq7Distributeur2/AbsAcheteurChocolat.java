6a76469f23be855aba115f8c6da24a1d5aaaa12c
Léandre Berthault <Landre Berthault@DESKTOP-NNH1GQP>
2020-05-18 20:03:55 +0200

    Ajout d'une classe Pair

--- src/abstraction/eq7Distributeur2/AbsAcheteurChocolat.java
+++ src/abstraction/eq7Distributeur2/AbsAcheteurChocolat.java
@@ -6,8 +6,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.sun.tools.javac.util.Pair;
-
 import abstraction.eq8Romu.produits.Chocolat;
 import abstraction.eq8Romu.produits.ChocolatDeMarque;
 import abstraction.fourni.Filiere;
--- src/abstraction/eq7Distributeur2/AcheteurChocolat.java
+++ src/abstraction/eq7Distributeur2/AcheteurChocolat.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.sun.tools.javac.util.List;
-import com.sun.tools.javac.util.Pair;
 
 import abstraction.eq8Romu.chocolatBourse.CommandeChocolat;
 import abstraction.eq8Romu.chocolatBourse.IAcheteurChocolatBourse;
