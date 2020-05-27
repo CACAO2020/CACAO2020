0eca15a8b1568965970f1794bdff604978e1eb98
lucas <lucas@LAPTOP-A41S86LO>
2020-05-27 12:57:48 +0200

    rectification de la production

--- src/abstraction/eq2Producteur2/PaquetArbres.java
+++ src/abstraction/eq2Producteur2/PaquetArbres.java
@@ -64,7 +64,7 @@ public class PaquetArbres {
 			return this.getProdmax();
 		}
 		else {
-			return(this.getProdmax()*(1-(this.getAge() - 30)/15));
+			return(this.getProdmax()*(1-(this.getAge() - 30)/45));
 		}
 		
 	}
