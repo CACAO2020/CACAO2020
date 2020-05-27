a42524aff2442bf74ecede77ae3323e380671129
kristof_sz <kristof.szentes@outlook.com>
2020-05-27 15:14:25 +0200

    ajout d'un journal d'achats

--- src/abstraction/eq2Producteur2/eq2Investisseur.java
+++ src/abstraction/eq2Producteur2/eq2Investisseur.java
@@ -1,5 +1,6 @@
 package abstraction.eq2Producteur2;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import abstraction.eq8Romu.cacaoCriee.IVendeurCacaoCriee;
@@ -13,18 +14,21 @@ import abstraction.eq8Romu.produits.Feve;
 
 public class eq2Investisseur extends eq2Vendeur {
 
-	private double prixArbre; 
+	private double prixArbre;
+	private Journal journal_achats;
 
 	public eq2Investisseur() {
 		super();
 		this.prixArbre = 3;
-		// TODO Auto-generated constructor stub
+		this.journal_achats = new Journal("Investissements",this);
 	}
 
 	public void AchatArbres(int nbrArbres, Feve feve){
 		if (Filiere.LA_FILIERE.getBanque().getSolde(this,this.getCrypto())> nbrArbres*this.prixArbre) {
 			this.ajoutPaquetArbres(new PaquetArbres(nbrArbres, feve));
 			Filiere.LA_FILIERE.getBanque().virer(this,this.getCrypto(),Filiere.LA_FILIERE.getBanque(),nbrArbres*this.prixArbre);
+			
+			this.journal_achats.ajouter("Achat de " + nbrArbres + " arbres de type: " + feve.getGamme());
 		}
 	}
 	/*
@@ -36,6 +40,12 @@ public class eq2Investisseur extends eq2Vendeur {
 	}
 	public double getprixArbre() {
 		return this.prixArbre;
+	}
+	public List<Journal> getJournaux() {
+		List<Journal> res=new ArrayList<Journal>();
+		res.addAll(super.getJournaux());
+		res.add(this.journal_achats);
+		return res;
 	}
 }
 
