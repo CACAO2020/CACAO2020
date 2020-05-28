e7d07cf3f7d68bd0d45ed6717de5a8d87fefc8c6
ACdr <ACdr@192.168.1.22>
2020-05-28 12:30:30 +0200

    mise en place de descisionTransformation

--- src/abstraction/eq3Transformateur1/Transformateur1.java
+++ src/abstraction/eq3Transformateur1/Transformateur1.java
@@ -13,10 +13,10 @@ import abstraction.fourni.Filiere;
 /** @author AMAURY COUDRAY*/
 public class Transformateur1 extends VendeurChocolat {
 	public Transformateur1() {
-		this.stockFeves.put(Feve.FEVE_MOYENNE,15.0);
+		this.stockFeves.put(Feve.FEVE_MOYENNE,150.0);
 		this.coutFeves.put(Feve.FEVE_MOYENNE, 6000.0);
 		this.MontantCompte=500000.0;
-		this.stockTotalFeves=new Variable("stock total de feves de "+getNom(),this,15.0);
+		this.stockTotalFeves=new Variable("stock total de feves de "+getNom(),this,150.0);
 		this.stockTotalChocolat=new Variable("stock total de chocolat de "+getNom(),this,0.0);
 		this.stockTotalPateInterne=new Variable("stock total de pate interne de "+getNom(),this,0.0);
 
@@ -24,18 +24,33 @@ public class Transformateur1 extends VendeurChocolat {
 	public Color getColor() {
 		return new Color(52, 152, 219);
 	}
-	public void next() {
+	public void descisionTransformation() {
 		for(Chocolat chocolat:this.getStockPateInterne().keySet()) {
-			this.transformationPateChocolat(chocolat, this.getStockPateInterne(chocolat));
+			if(this.getStockPateInterne(chocolat)*4000<=this.getMontantCompte()){
+				this.transformationPateChocolat(chocolat, this.getStockPateInterne(chocolat));
+			}
+			else {
+				this.transformationPateChocolat(chocolat, this.getMontantCompte()/4000);
+			}
 		}
 		for(Feve feve:this.getStockFeves().keySet()) {
-			this.transformationFevePate(feve, this.getStockFeves(feve));
+			if(this.getStockFeves(feve)*8000<=this.getMontantCompte()) {
+				this.transformationFevePate(feve, this.getStockFeves(feve));
+			}
+			else {
+				this.transformationFevePate(feve, this.getMontantCompte()/8000);
+			}
 		}
+	}
+	public void next() {
+		this.descisionTransformation();
 		System.out.println("cout des feves"+this.getCoutFeves());
 		System.out.println("cout des pates internes"+this.getCoutPateInterne());
 		System.out.println("cout des chocolat"+this.getCoutChocolat());
 		System.out.println("stock chocoalt"+this.getStockChocolat());
+		System.out.println("treso "+this.getMontantCompte());
 	}
+	
 	public List<Variable> getIndicateurs() {
 		List<Variable> res=new ArrayList<Variable>();
 		res.add(this.stockTotalFeves);
