21467cc1c8d678668c23c5f2adf007cafe678178
thiba <thiba@192.168.1.128>
2020-06-05 11:49:13 +0200

    Début V2 - Mélissa, Martial et Thibault

--- src/abstraction/eq6Distributeur1/Distributeur1.java
+++ src/abstraction/eq6Distributeur1/Distributeur1.java
@@ -6,14 +6,13 @@ import abstraction.eq8Romu.produits.ChocolatDeMarque;
 
 public class Distributeur1 extends DistributeurClientFinal {
 
-	public Distributeur1(double capaciteDeVente, double margeHGE, double margeMG, double margeBG,
-			double capaciteStockmax, double pctageHGE, double pctageMG, double pctageBG) {
-		super(capaciteDeVente, margeHGE, margeMG, margeBG, capaciteStockmax, pctageHGE, pctageMG, pctageBG);
+	public Distributeur1(double capaciteDeVente, double marge, double capaciteStockmax, double pctageHGE, double pctageMG, double pctageBG) {
+		super(capaciteDeVente, marge, capaciteStockmax, pctageHGE, pctageMG, pctageBG);
 		// TODO Auto-generated constructor stub
 	}
 	
 	public Distributeur1() {
-		super(100000, 0.5, 0.4, 0.2, 10000000, 0.15, 0.35, 0.50);
+		super(100000, 1.3, 10000000, 0.15, 0.35, 0.50);
 	}
 
 }
