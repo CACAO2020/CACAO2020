e1c55243c6c34de9928bc69a556c75c87d05a403
francoisgoujon <francoisgoujon@wanadoo.fr>
2020-06-12 11:46:11 +0200

    méthode avis

--- src/abstraction/eq5Transformateur3/SuiviDesCoursDeVente.java
+++ src/abstraction/eq5Transformateur3/SuiviDesCoursDeVente.java
@@ -9,6 +9,12 @@ import java.util.Map;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+
+/** @author francoisgoujon 
+ * 
+ *
+ */
+
 public class SuiviDesCoursDeVente {
 	
 	private Transformateur3 acteur;
@@ -22,7 +28,7 @@ public class SuiviDesCoursDeVente {
 	public SuiviDesCoursDeVente(Transformateur3 acteur) {
 		this.acteur = acteur;
 		this.prixStat = new HashMap<Chocolat, List<Double>>();
-		this.tour = Filiere.LA_FILIERE.getEtape();
+		this.tour = 0;
 		this.prixStat.put(Chocolat.CHOCOLAT_BASSE, new ArrayList<Double>(3));
 		this.prixStat.put(Chocolat.CHOCOLAT_MOYENNE, new ArrayList<Double>(3));
 		this.prixStat.put(Chocolat.CHOCOLAT_HAUTE, new ArrayList<Double>(3));
@@ -35,30 +41,84 @@ public class SuiviDesCoursDeVente {
 			this.prixStat.get(choco).set(2, 0.0);
 		}
 	}
+	public void next() {
+		this.tour = this.tour + 1;
+		
+		this.updatePrixStat();
+	}
 	
-	public Double getCours(Chocolat choco, int n) {
+	public double getCours(Chocolat choco, int n) {
 		return Filiere.LA_FILIERE.getIndicateur("BourseChoco cours " + choco.name()).getHistorique().get(n).getValeur();	
 	}
-	public Double getCours(Chocolat choco) {
+	public double getCours(Chocolat choco) {
 		return Filiere.LA_FILIERE.getIndicateur("BourseChoco cours " + choco.name()).getHistorique().get(Filiere.LA_FILIERE.getEtape()).getValeur();
 	}
+	public double getMax(Chocolat choco) {
+		return this.prixStat.get(choco).get(1);
+	}
+	public double getMin(Chocolat choco) {
+		return this.prixStat.get(choco).get(0);
+	}
+	public double getMoy(Chocolat choco) {
+		return this.prixStat.get(choco).get(2);
+	}
+
 	
 	public void updatePrixStat(Chocolat choco) {
-		Double cours = Filiere.LA_FILIERE.getIndicateur("BourseChoco cours " + choco.name()).getHistorique().get(this.tour).getValeur();
-		if (cours < this.prixStat.get(choco).get(0) ) {
+		double cours = this.getCours(choco);
+		if (cours <= this.prixStat.get(choco).get(0) ) {
 			this.prixStat.get(choco).set(0, cours);
 		}
-		if (cours > this.prixStat.get(choco).get(1) ) {
+		if (cours >= this.prixStat.get(choco).get(1) ) {
 			this.prixStat.get(choco).set(1, cours);
 		}
-		Double moyenne = this.prixStat.get(choco).get(2);
-		Double nouvelleMoyenne = moyenne*(this.tour/(this.tour+1)) + cours/(this.tour+1);
+		double moyenne = this.getMoy(choco);		
+		double nouvelleMoyenne = moyenne*(this.tour/(this.tour+1)) + cours/(this.tour+1);
 		this.prixStat.get(choco).set(2, nouvelleMoyenne);
 	}
+	/**
+	 * 
+	 * @return une map à 5 entrée, une pour chaque type de chocolat, avec pour chaque entrée : 0 si le cours est dans la moyenne, 1 si le cours est en dessous du min
+	 */
+	
+	
 	public void updatePrixStat() {
 		for (Chocolat choco : Chocolat.values()) {
 			updatePrixStat(choco);
 		}
+	}
+	/** 
+	 * Renvoie le cours en poucentage de la moyenne (renvoie 1 si les cours = moyenne)
+	 */
+	public double getCours_Moyenne(Chocolat choco) {
+		Double moy = this.getMoy(choco);
+		Double cours = this.getCours(choco);
+		return cours/moy;
+	}
+	/**
+	 * 
+	 * @param choco
+	 * @return 0 si le prix est entre min et max : indifférent
+	 * 			1 si le prix est < min ou < 1/2 moyenne : pas d'achat
+	 * 			2 si le prix est > max ou > 2 moyenne : achat
+	 */
+	public int avisAchat(Chocolat choco) {
+		double cours = this.getCours(choco);
+		int res = 0;
+		if (cours <= this.prixStat.get(choco).get(0) || this.getCours_Moyenne(choco) <= 0.5) {
+			res = 1;
+		}
+		if (cours >= this.prixStat.get(choco).get(1) || this.getCours_Moyenne(choco) >= 2) {
+			res = 2;
+		}
+		return res;
+	}
+	public HashMap<Chocolat, Integer> avisAchat() {
+		HashMap<Chocolat, Integer> map = new HashMap<Chocolat, Integer>();
+		for (Chocolat choco : Chocolat.values()) {
+			map.put(choco,this.avisAchat(choco));
+		}
+		return map;
 	}
 	
 
